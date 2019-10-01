edfe8a432737b4ef1fe4c889b6891555e12dcba7
Walter Johnson <johnwalt@oregonstate.edu>
2019-10-01 06:43:47 -0700

    testing floating point calculations

--- arrayloop.sh
+++ arrayloop.sh
@@ -1,6 +1,6 @@
 # arrayLoop
 #!/bin/bash
-array= ( one two three ) # declares and fills array
+array=( one two three ) # declares and fills array
 for i in "${array[@]}" # array[n] fetches n-th element in array, array[@] fetches ALL elements in array
 do
     echo $i
--- countdividelooptest.sh
+++ countdividelooptest.sh
@@ -5,11 +5,16 @@ sum=0
 count=0
 for i in $myLine
 do
+#    echo "i is: $i"
     sum=`expr $sum + $i`
+#    echo "sum is: $sum"
     count=`expr $count + 1`
+#    echo "count is: $count"
 done
 mean=`expr $sum / $count`
 echo "mean average is $mean"
+printf "mean average is "
+bc <<< "scale=4;$sum/$count"
 
 # use: cat <datafile> | countdividelooptest.sh
 # returns integer only. expr only wirks with integers
