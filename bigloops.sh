aff552f6641ed2a2611aebb99ed52ec63cafeb07
waltercj3 <waltercj3@gmail.com>
2019-10-03 15:35:03 -0700

    completed bigloops.sh

--- bigloops.sh
+++ bigloops.sh
@@ -7,4 +7,20 @@ do
 	sum=0
 	count=0
 	average=0
-	lineNum=`expr $lineNum + 1
+	lineNum=`expr $lineNum + 1`
+	echo "processing line $lineNum: $myLine"
+
+	for i in $myLine
+	do
+		num=$i
+		count=`expr $count + 1`
+		echo "  Item $count: $num"
+		sum=`expr $sum + $num`
+	done
+
+	average=`expr $sum / $count`
+
+	echo "  Line count: $count"
+	echo "  Line sum: $sum"
+	echo "  Line average: $average"
+done < $1
