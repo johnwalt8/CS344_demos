2f6e1188f0a7a4091f5330d80924b2c4fb9dcc84
Walter Johnson <johnwalt@oregonstate.edu>
2019-10-03 17:25:27 -0700

    fixed stuff

--- exitStatus.sh
+++ exitStatus.sh
@@ -1,6 +1,7 @@
 # exitStatus.sh
 #!/bin/bash
 echo "About to do an 'ls > /dev/null'"
+ls > /dev/null
 printf "Result of ls was: $?\n"
 echo "About to exit with status 5, use 'echo \$?' to see it"
 exit 5
